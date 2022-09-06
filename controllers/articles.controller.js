e33cbf63f06c5010e8413b50432dc44f6cc54ff8
caitlin-fin <c8linfin@gmail.com>
2022-09-06 14:31:02 +0100

    add check for invalid article_id test for GET /api/articles/:article_id

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -2,12 +2,18 @@ const { selectArticle } = require("../models/articles.model");
 
 exports.getArticle = (req, res, next) => {
   const { article_id } = req.params;
-  selectArticle(article_id)
-    .then((output) => {
+  console.log(article_id, isNaN(article_id));
+
+  if (isNaN(article_id)) {
+    res.status(404).send({ msg: "article_id not valid" });
+
+  } else {
+    selectArticle(article_id).then((output) => {
       if (output === undefined) {
-        res.status(404).send({msg: "article doesn't exist"})
+        res.status(404).send({ msg: "article doesn't exist" });
       } else {
-      res.status(200).send({ article: output });
+        res.status(200).send({ article: output });
       }
-    })
+    });
+  }
 };
