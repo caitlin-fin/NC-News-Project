86c0e78cc3a861de9a2af08e196dce20ff8039dc
caitlin-fin <c8linfin@gmail.com>
2022-09-06 15:14:20 +0100

    rename variable in users.controller.js

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,7 +1,7 @@
 const { selectUsers } = require("../models/users.model");
 
 exports.getUsers = (req, res) => {
-  selectUsers().then((output) => {
-    res.status(200).send({ users: output });
+  selectUsers().then((users) => {
+    res.status(200).send({ users: users });
   });
 };
