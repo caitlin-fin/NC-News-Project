96d785f931cfe32f93dde7b77d2558a512973e46
caitlin-fin <c8linfin@gmail.com>
2022-09-27 10:25:14 +0100

    add cors

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+const cors = require('cors');
+
+app.use(cors())
 
 const { getTopics } = require("./controllers/topics.controller");
 const {
